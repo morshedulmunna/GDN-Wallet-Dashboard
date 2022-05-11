14c44c598436c5df24ed47b673e29428d4f7c158
morshedulmunna <morshedulmunna1@gmail.com>
2022-05-11 14:36:21 +0600

    sidebar complete

--- src/Shared/Sidebar/Sidebar.jsx
+++ src/Shared/Sidebar/Sidebar.jsx
@@ -8,6 +8,7 @@ import {
   transactions,
   statistics,
 } from "../../Assect/Icons/icons";
+import mobileApp from "../../Assect/Images/mobileApp.png";
 
 const menu = [
   {
@@ -44,20 +45,28 @@ const menu = [
 
 const Sidebar = () => {
   return (
-    <div className="border-r-[1px] border-r-gray-300 h-[100vh] ">
-      <div>
-        <div className={styles.logo}>
-          <img src={logo} alt="" />
-          <p className="pl-3">GND WALLET</p>
+    <div className={styles.sidebar}>
+      <div className={styles.wrapper}>
+        <div>
+          <div className={styles.logo}>
+            <img src={logo} alt="" />
+            <p className="pl-3">GND WALLET</p>
+          </div>
+          {/* Menu==> */}
+          <div className="menu">
+            {menu.map((sideNav) => (
+              <div key={sideNav.id} className={styles.sideNav}>
+                <img src={sideNav.icon} alt="" />
+                <p className={styles.name}> {sideNav.name} </p>
+              </div>
+            ))}
+          </div>
         </div>
-        {/* Menu==> */}
-        <div className="menu">
-          {menu.map((menuItem) => (
-            <div key={menuItem.id} className={styles.dashboard}>
-              <img src={menuItem.icon} alt="" />
-              <p className="pl-5 text-gray-400"> {menuItem.name} </p>
-            </div>
-          ))}
+        <div className={styles.appdowenload}>
+          <div>
+            <img className="mt-[-100px]" src={mobileApp} alt="" />
+            <button className={styles.dowenloadBtn}>Dowenload</button>
+          </div>
         </div>
       </div>
     </div>
@@ -67,8 +76,14 @@ const Sidebar = () => {
 export default Sidebar;
 
 // All Style
-
 const styles = {
+  sideNav: "flex items-center justify-start px-12 py-5 ",
+  wrapper: " flex flex-col justify-between h-[100vh] ",
   logo: "logo flex items-center justify-start py-10 px-12",
-  dashboard: "flex items-center justify-start px-12 py-5",
+  sidebar: "border-r-[1px] border-r-gray-800 h-[100vh]",
+  name: "cursor-pointer pl-5 text-gray-400",
+  appdowenload:
+    "bg-gray-700 w-[70%] rounded-lg mx-auto flex items-center justify-center py-5 mb-5",
+  dowenloadBtn:
+    "mt-5 w-full bg-[#6855FC]  py-1 rounded-full hover:bg-[#5242cc] ease-in-out duration-300",
 };
